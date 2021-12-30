dfb7bfaa3301519bcfdf9eda2ef09164e1bc60b0
SEDO11 <do11anm@naver.com>
2021-12-30 15:36:56 +0900

    211230 노마드 코더 1

---
+++ app.js
@@ -0,0 +1,38 @@
+// const a = 5
+// const b = 2
+// let myName = "SeHoon"
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(myName)
+// myName = "OH"
+// console.log(myName)
+
+
+// let day = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+// let text = ['', '']
+// for (let i = 0; i < day.length; i++) {
+//     text[0] += day[i] + " "
+// }
+// console.log(day)
+// console.log(text[0])
+// day.push("hello")
+// for (let i = 0; i < day.length; i++) {
+//     text[1] += day[i] + " "
+// }
+// console.log(day)
+// console.log(text[1])
+
+
+//오브젝트
+const player = {
+    name: "nico",
+    points: 10,
+    fat: true,
+}
+console.log(player)
+console.log(player.name)
+player.lastName = "potato"
+player.points = 15
+console.log(player)
