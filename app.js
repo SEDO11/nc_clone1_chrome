0830fa045b44a93409c1fab76e6c1b027d261519
SEDO11 <do11anm@naver.com>
2022-01-22 09:05:27 +0900

    220122

--- app.js
+++ app.js
@@ -47,22 +47,66 @@
 // hello("dico", 20)
 // hello("minji", 35)
 
-const player = {
-    name: "nico",
-    sayHello: function(name) {
-        console.log("hello " + name + " nice meet you ")
-        document.write("hello " + name + " nice meet you! ")
-    },
+// const player = {
+//     name: "nico",
+//     sayHello: function(name) {
+//         console.log("hello " + name + " nice meet you ")
+//         document.write("hello " + name + " nice meet you! ")
+//     },
+// }
+
+// console.log(player.name)
+// player.sayHello("lynn")
+// player.sayHello("dico")
+// player.sayHello("suzan")
+
+// const test = {
+//     myName: function (name) {
+//       document.write('Hello My is ' + name + ' nice meet you')  
+//     },
+// }
+// test.myName("Hoon")
+
+let result = document.getElementById('result')
+let sub = document.getElementById('sub')
+
+let num1 = 0
+let num2 = 0
+
+function inputNum() {
+    num1 = window.Number(prompt('첫번째 숫자를 입력하세요'))
+    num2 = window.Number(prompt('두번째 숫자를 입력하세요'))
 }
 
-console.log(player.name)
-player.sayHello("lynn")
-player.sayHello("dico")
-player.sayHello("suzan")
+const cal = {
 
-const test = {
-    myName: function (name) {
-      document.write('Hello My is ' + name + ' nice meet you')  
+    add: function () {
+        let addResult = num1 + num2
+        sub.innerHTML = '(덧셈)'
+        result.innerHTML = addResult
     },
+
+    min: function () {
+        let minResult = num1 - num2
+        sub.innerHTML = '(뺄셈)'
+        result.innerHTML = minResult
+            },
+
+    mun: function () {
+        let munResult = num1 * num2
+        sub.innerHTML = '(곱셈)'
+        result.innerHTML = munResult
+            },
+
+    divi: function () {
+        let diviResult = num1 / num2
+        sub.innerHTML = '(나눗셈)'
+        result.innerHTML = diviResult
+            },
+
+    squ: function () {
+        let squResult = num1 ** num2
+        sub.innerHTML = '(제곱)'
+        result.innerHTML = squResult
+        },
 }
-test.myName("Hoon")
